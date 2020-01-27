8c5e4fd02cf4b0ac1dc4a49b84d7ff4404438a77
ffkk <fumihiko.kamii775@gmail.com>
2020-01-27 21:17:55 +0900

    second result4

--- bmi_class.py
+++ bmi_class.py
@@ -16,12 +16,19 @@ class BMI:
     def __init__(self, height, weight):
         self.value = weight / (height ** 2)
 
+        if not (10 <= self.value <= 40):
+            raise ValueError('BMIの値が正常値の範囲外です')
+
     def __str__(self):
         return f'{self.value:.2f}'
+
 
 # BMIクラスのインスイタンス化
 hibiki_bmi = BMI(height=1.80, weight=67.0)
 print(hibiki_bmi)
 
 ohira_bmi = BMI(height=1.78, weight=75.0)
+print(ohira_bmi)
+
+yabai_bmi = BMI(height=1.78, weight=75000.0)
 print(ohira_bmi)
